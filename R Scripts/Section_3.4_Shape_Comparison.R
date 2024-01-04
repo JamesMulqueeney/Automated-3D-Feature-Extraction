093e8879092caa071fc0770d1e47e2cecee1e44b
James Mulqueeney <122978194+JamesMulqueeney@users.noreply.github.com>
2024-01-04 17:15:49 +0000

    Update Section_3.4_Shape_Comparison.R

--- R Scripts/Section_3.4_Shape_Comparison.R
+++ R Scripts/Section_3.4_Shape_Comparison.R
@@ -23,7 +23,7 @@ library(dplyr)
 # Internal Shape #
 
 # Read in the Internal shape .csv data 
-Internal.Shape <- read.csv("D:/CTData/James Mulqueeney/Papers/Write Up Papers/Paper 1- 3D AI Augmentation Paper/Dice Similarity Coefficient Results/Test Data Results/Internal Mesh Shape/Attempt 2/2023-10-13_Internal_Shape_Comparison_v3.csv")
+Internal.Shape <- read.csv("/path/to/your/directory/Attempt 2/2023-10-13_Internal_Shape_Comparison_v3.csv")
 
 #########################################################################################
 
