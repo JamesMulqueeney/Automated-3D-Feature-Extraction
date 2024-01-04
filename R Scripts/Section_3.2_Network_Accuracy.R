62d9ef1038cf63d54b9b838837c54ad84f068867
James Mulqueeney <122978194+JamesMulqueeney@users.noreply.github.com>
2024-01-04 17:59:18 +0000

    Update Section_3.2_Network_Accuracy.R

--- R Scripts/Section_3.2_Network_Accuracy.R
+++ R Scripts/Section_3.2_Network_Accuracy.R
@@ -23,7 +23,7 @@ library(dplyr)
 # Read in Data & Set up Orders # 
 
 # Load in Dice Score .csv data 
-Dice <- read.csv("/path/to/your/directory/2023-07-05_J_Mul_Dice_v2.csv")
+Dice <- read.csv("/path/to/your/directory/Dice_Score_Results.csv")
 
 # Subset the data for Original and Augmentation Data 
 original_subset <- subset(Dice, Dice$Data.Type == "Original") # subset for original data
